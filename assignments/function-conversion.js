029e1782c823786d5e0bcb946b451b275fecf98d
Brownbatz <Lopeztgabriel4@gmail.com>
2019-09-04 19:20:07 -0600

    I have hit MVP for this project

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,21 +4,38 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+const log = (string) => {
+    return console.log(string);
+}
+log("Function was invoked");
+
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+const anotherFunction = (anotherString) => {
+    return anotherString;
+}
+anotherFunction("Testing out another string.");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+const add = (a,b) => {
+    return a + b;
+}
+console.log(add(5, 2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+const subtract = (a,b) => {
+    return a - b;
+}
+console.log(subtract(5, 2));
 
 
 // Stretch
